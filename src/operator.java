148802024d7672b693d14ad192df7bb8554f8d5d
Moktan Sujita <sujitamoktan77@gmail.com>
2025-01-26 18:58:11 +0545

    first commit

---
+++ src/operator.java
@@ -0,0 +1,16 @@
+public class operator {
+    public static void main(String []args)
+    {
+        int a=50;
+        int b=50;
+        int c=a+b;
+        System.out.println("c=a+b="+c);
+    System.out.println("c=a-b="+(a-b));
+    System.out.println("c=a%b="+(a%b));
+    System.out.println("c=a/b="+(a/b));
+    System.out.println("c=a*b="+(a*b));
+    System.out.println("c=a++="+(a++));
+    System.out.println("c=b++"+(b++));
+    
+    }
+}
