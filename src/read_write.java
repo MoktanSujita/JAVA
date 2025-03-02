47c49a74ef54698289ad2ac255a49ccfe2b4fb73
Moktan Sujita <sujitamoktan77@gmail.com>
2025-03-02 07:09:46 +0545

    commit

---
+++ src/read_write.java
@@ -0,0 +1,26 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class read_write {
+    public static void main(String[] args) throws Exception 
+    {
+        FileReader in=null;
+        FileWriter out= null;
+         try {
+            in=new FileReader("input.txt");
+            out=new FileWriter("output.txt");
+            int c;
+            while ((c=in.read())!=-1) 
+            {
+                out.write(c);
+            }
+         } 
+         finally
+         {
+            in.close();
+            out.close();
+         }
+         }
+    }
+    
+}
